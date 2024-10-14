d115e7948ce3c89b239da9b34648e7e0c8f98406
heidaraliy <heidar.aliy@gmail.com>
2024-10-14 16:56:36 -0400

    restructured front end for additional separation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AudioUploader from './components/AudioUploader/AudioUploader';
 
 function App() {
   return (
-    <div className="bg-[#B79CED] h-screen-max p-4 sm:p-16 lg:p-24 ">
+    <div className="bg-[#231942] h-screen-max p-4 sm:p-8 lg:p-12 ">
       <AudioUploader />
     </div>
   );
