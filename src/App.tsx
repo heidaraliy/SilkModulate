6c239fdf5630c341b8616ff88ee29c084e1fcae6
heidaraliy <heidar.aliy@gmail.com>
2024-12-16 07:55:20 -0500

    actually add bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AudioUploader from './components/AudioUploader/AudioUploader';
 
 function App() {
   return (
-    <div className="bg-[#4b56a0]h-screen-max p-4 sm:p-8 lg:p-12">
+    <div className="bg-[#4b56a0] h-screen-max p-4 sm:p-8 lg:p-12">
       <AudioUploader />
     </div>
   );
