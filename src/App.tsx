0c2433fc993fdc8f8acff1555539b54c6375a1e2
heidaraliy <heidar.aliy@gmail.com>
2024-12-16 07:53:26 -0500

    bg change (again)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import AudioUploader from './components/AudioUploader/AudioUploader';
-import background from './assets/background.png';
 
 function App() {
   return (
-    <div
-      className="bg-cover bg-center bg-no-repeat h-screen-max p-4 sm:p-8 lg:p-12"
-      style={{ backgroundImage: `url(${background})` }}
-    >
+    <div className="bg-[#4b56a0]h-screen-max p-4 sm:p-8 lg:p-12">
       <AudioUploader />
     </div>
   );
